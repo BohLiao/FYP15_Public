8ca9b847cf9ea204668e06505a90e162d0d0efaf
BohLiao <woowunglai@gmail.com>
2025-07-28 17:06:26 +0800

    Configured TiDB and updated Render deployment settings

--- backend/index.js
+++ backend/index.js
@@ -35,10 +35,13 @@ app.use(bodyParser.json());
 // MySQL connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: true },
+  ssl: {
+    ca: fs.readFileSync(process.env.SSL_CA),
+  },
 });
 
 db.connect((err) => {
